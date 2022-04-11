2fa84e45985ed941cc3e6a08e5d5bea9706f1c6e
Nikitaroy31 <otpchatappservice@gmail.com>
2022-04-11 20:39:03 +0530

    ended.....

--- driver/utils.py
+++ driver/utils.py
@@ -122,13 +122,19 @@ async def left_handler(_, chat_id: int):
         await calls.leave_group_call(chat_id)
         await remove_active_chat(chat_id)
         clear_queue(chat_id)
-        
+
+############
+# 
+#
+#        
 @Client.on_message(filters.voice_chat_ended)
 async def closed_voice_chathandler(_, chat_id: int):
+    m = await Client.send_message('voice chat ended')
     if chat_id in QUEUE:
         await calls.leave_group_call(chat_id)
         await remove_active_chat(chat_id)
         clear_queue(chat_id)
+        await m.edit('cleared queue')
 
 @calls.on_stream_end()
 async def stream_end_handler(_, u: Update):
@@ -139,7 +145,7 @@ async def stream_end_handler(_, u: Update):
             await calls.leave_group_call(chat_id)
             await remove_active_chat(chat_id)
             clear_queue(chat_id)
-            #return
+            return
         elif queue == 2:
             await bot.send_message(
                 chat_id,
