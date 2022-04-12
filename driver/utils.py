53b5232da05e4705b7336df8ad1974e0169babec
Nikitaroy31 <92322219+Nikitaroy31@users.noreply.github.com>
2022-04-12 07:44:57 +0530

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -129,7 +129,7 @@ async def left_handler(_, chat_id: int):
        
 @user.on_message(filters.voice_chat_ended)
 async def closed_voice_chathandler(_, chat_id: int):
-    m = await user.send_message('voice chat ended')
+    m = await user.send_message(chat_id,'voice chat ended')
     if chat_id in QUEUE:
         await calls.leave_group_call(chat_id)
         await remove_active_chat(chat_id)
